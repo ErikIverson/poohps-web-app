3b7e7c57ac0547a74008cfdf2b61aab671724aeb
Erik Iverson <erik.iverson@drake.edu>
2021-08-29 13:11:22 -0500

    login button and route

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HeaderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
